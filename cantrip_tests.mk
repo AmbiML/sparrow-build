34d6c0b16164551427c8f43896a97e618f6cb816
June Tate-Gans <jtgans@google.com>
2023-07-06 16:16:33 +0000

    Revert "Revert "build: add targets for driver unit tests""
    This reverts commit 5d6c3c624f299e847ee045e0f2cfb51885d2ce34 and fixes
    the underlying dependency issue.
    
    Reason for revert: Just need some additional build deps.
    
    Change-Id: Ica73ff86793567905cceae91887a21197ab3cfd2
    GitOrigin-RevId: 33e1832de48596c5764b885ad00cb24c5ce7edf1

--- cantrip_tests.mk
+++ cantrip_tests.mk
@@ -21,31 +21,51 @@ CARGO_TEST        := ${CARGO_CMD} test
 #	cargo_test_cantrip_os_common_logger &
 #   cargo_test_cantrip_proc_interface; they need to be rewritten (or tossed)
 CARGO_TEST_CANTRIP=\
-	cargo_test_cantrip_os_common_slot_allocator
+  cargo_test_cantrip_os_common_slot_allocator \
+  cargo_test_mailbox_driver \
+  cargo_test_opentitan_timer \
+  cargo_test_uart_driver \
+  cargo_test_cantrip_vec_core
 
 ## Runs all cargo unit tests for the Cantrip operating system
-cargo_test_cantrip: $(CARGO_TEST_CANTRIP)
+cargo_test_cantrip: $(CARGO_TEST_CANTRIP) | cantrip-gen-headers
 
 ## Runs cargo unit tests for the ProcessManager implementation
-cargo_test_cantrip_proc_manager:
+cargo_test_cantrip_proc_manager: | cantrip-gen-headers
 	cd $(CANTRIP_COMPONENTS)/ProcessManager/cantrip-proc-manager && $(CARGO_TEST)
 
 ## Runs cargo unit tests for the ProcessManager interface
-cargo_test_cantrip_proc_interface:
+cargo_test_cantrip_proc_interface: | cantrip-gen-headers
 	cd $(CANTRIP_COMPONENTS)/ProcessManager/cantrip-proc-interface && $(CARGO_TEST)
 
 ## Runs cargo unit tests for the CantripLogger service
-cargo_test_cantrip_os_common_logger:
+cargo_test_cantrip_os_common_logger: | cantrip-gen-headers
 	cd $(CANTRIP_COMPONENTS)/cantrip-os-common/src/logger && \
 		$(CARGO_TEST) -- --test-threads=1
 
 ## Runs cargo unit tests for the CantripSlotAllocator crate
-cargo_test_cantrip_os_common_slot_allocator:
+cargo_test_cantrip_os_common_slot_allocator: | cantrip-gen-headers
 	cd $(CANTRIP_COMPONENTS)/cantrip-os-common/src/slot-allocator && \
 		$(CARGO_TEST) -- --test-threads=1
 
 ## Runs cargo unit tests for the DebugConsole zmomdem support
-cargo_test_debugconsole_zmodem:
+cargo_test_debugconsole_zmodem: | cantrip-gen-headers
 	cd $(CANTRIP_COMPONENTS)/DebugConsole/zmodem && $(CARGO_TEST)
+
+## Runs cargo unit tests for the MailboxDriver
+cargo_test_mailbox_driver: | cantrip-gen-headers
+	cd $(CANTRIP_COMPONENTS)/MailboxDriver/tests && $(CARGO_TEST)
+
+## Runs cargo unit tests for the opentitan-timer support
+cargo_test_opentitan_timer: | cantrip-gen-headers
+	cd $(CANTRIP_COMPONENTS)/TimerService/tests && $(CARGO_TEST)
+
+## Runs cargo unit tests for the UARTDriver
+cargo_test_uart_driver: | cantrip-gen-headers
+	cd $(CANTRIP_COMPONENTS)/UARTDriver/tests && $(CARGO_TEST)
+
+## Runs cargo unit tests for the Vector Core support
+cargo_test_cantrip_vec_core: | cantrip-gen-headers
+	cd $(CANTRIP_COMPONENTS)/MlCoordinator/tests && $(CARGO_TEST)
 
 .PHONY:: cargo_test_cantrip $(CARGO_TEST_CANTRIP)
