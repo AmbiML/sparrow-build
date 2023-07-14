0bae061593ba2057310765a8fa87afc0669f6d77
Sam Leffler <sleffler@google.com>
2023-07-14 14:03:06 -0700

    sparrow: use top_matcha hjson as SOT for user mode code
    Generate top_matcha_{irq,memory}.h for use by system.camkes instead of
    duplicating hw memory addresses, region sizes, and irq assignments.
    
    TBD: generate rust defs for MlCoordinator (or remove use)
    TBD: generate kernel definitions (esp dts)
    
    Bug: 276823440
    
    Change-Id: I82cdddd38f0fe8fc74586e5b2831a43842b4ece8
    GitOrigin-RevId: 6f90b28c1efb17ad077c9f17b041f22b06f4a304

--- platforms/sparrow/setup.sh
+++ platforms/sparrow/setup.sh
@@ -42,6 +42,8 @@ export ML_TOP_HJSON="${ROOTDIR}/hw/matcha/hw/top_matcha/ip/ml_top/data/ml_top.hj
 export TIMER_HJSON="${OPENTITAN_GEN_DIR}/rv_timer.hjson"
 export UART_HJSON="${OPENTITAN_SOURCE}/hw/ip/uart/data/uart.hjson"
 export VC_TOP_HJSON="${ROOTDIR}/hw/matcha/hw/top_matcha/ip/vc_top/data/vc_top.hjson"
+# Input for topgen_matcha.py to generate hw configuration
+export TOP_MATCHA_HJSON="${ROOTDIR}/hw/matcha/hw/top_matcha/data/top_matcha.hjson"
 
 function parting_messages() {
     if [[ ! -d "${RUSTDIR}" ]] ||
