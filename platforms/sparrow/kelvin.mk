644f757c0346677ee4897c08a1b364fbbb9286cc
Cindy Liu <hcindyl@google.com>
2023-07-31 20:47:25 +0000

    Merge "Add kelvin SW test target"
    GitOrigin-RevId: eb8cce8b2cd4853d0c94b1828e43275deb9b240e

--- platforms/sparrow/kelvin.mk
+++ platforms/sparrow/kelvin.mk
@@ -3,6 +3,7 @@
 KELVIN_SW_SRC_DIR := $(ROOTDIR)/sw/kelvin
 KELVIN_SW_OUT_DIR := $(OUT)/kelvin/sw
 KELVIN_SW_BAZEL_OUT_DIR := $(KELVIN_SW_OUT_DIR)/bazel_out
+KELVIN_SW_TESTLOG_DIR := $(KELVIN_SW_OUT_DIR)/bazel_testlog
 
 KELVIN_SIM_SRC_DIR := $(ROOTDIR)/sim/kelvin
 KELVIN_SIM_OUT_DIR := $(OUT)/kelvin/sim
@@ -11,6 +12,9 @@ KELVIN_SIM_OUT_DIR := $(OUT)/kelvin/sim
 $(KELVIN_SW_OUT_DIR):
 	mkdir -p "$(KELVIN_SW_OUT_DIR)"
 	mkdir -p "$(KELVIN_SW_BAZEL_OUT_DIR)"
+
+$(KELVIN_SW_TESTLOG_DIR):
+	mkdir -p "$(KELVIN_SW_TESTLOG_DIR)"
 
 ## Build Kelvin SW artifacts
 #
@@ -24,8 +28,16 @@ kelvin_sw: | $(KELVIN_SW_OUT_DIR)
 			-wholename "*ST-*/*.bin" \) \
 			-exec cp -f {} "$(KELVIN_SW_BAZEL_OUT_DIR)/" \;
 
+## Test Kelvin SW artifacts
+#
+# Test Kelvin SW artifacts with kelvin ISS simulation
+kelvin_sw_test: kelvin_sim | $(KELVIN_SW_TESTLOG_DIR)
+	cd "$(KELVIN_SW_SRC_DIR)"; \
+		bazel test --test_env=ROOTDIR=$(ROOTDIR) //... ; \
+		cp -rf bazel-testlogs/tests "$(KELVIN_SW_TESTLOG_DIR)"
+
 ## Clean Kelvin SW artifacts
-kelvin_clean:
+kelvin_sw_clean:
 	rm -rf "$(KELVIN_SW_OUT_DIR)"
 	cd "$(KELVIN_SW_SRC_DIR)" && bazel clean --expunge
 
@@ -42,6 +54,13 @@ kelvin_sim: | $(KELVIN_SIM_OUT_DIR)
 	cd "$(KELVIN_SIM_SRC_DIR)/bazel-bin" && \
 		cp -f sim/kelvin_sim "$(KELVIN_SIM_OUT_DIR)"
 
+## Clean Kelvin ISS
+#
+# Clean the Kelvin ISS
+kelvin_sim_clean:
+	cd "$(KELVIN_SIM_SRC_DIR)" && \
+		bazel clean --expunge
+	rm -rf $(KELVIN_SIM_OUT_DIR)
 
-PHONY:: kelvin_sw kelvin_clean
-PHONY:: kelvin_sim
+PHONY:: kelvin_sw kelvin_sw_clean kelvin_sw_test
+PHONY:: kelvin_sim kelvin_sim_clean
